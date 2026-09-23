39ec791be4cd5459f4bd18a4541cb2e2655bac40
Mike Accolade <growwithaccolade@gmail.com>
2026-09-24 00:26:22 +0100

    Add persistent cart and checkout foundation

---
+++ app/cart/page.tsx
@@ -0,0 +1,6 @@
+import CommerceHeader from "@/components/commerce-header";
+import CartPageClient from "@/components/cart-page";
+
+export default function CartPage() {
+  return <><CommerceHeader/><CartPageClient/></>;
+}
