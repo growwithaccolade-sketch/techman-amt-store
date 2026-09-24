952ffd0f1cb281f8fa111e5a4c17a7c35670ae08
Mike Accolade <growwithaccolade@gmail.com>
2026-09-24 06:27:55 +0100

    Use editable announcement in commerce header

--- components/commerce-header.tsx
+++ components/commerce-header.tsx
@@ -5,10 +5,10 @@ import { ShoppingBag } from "lucide-react";
 import { useCart } from "@/components/cart-provider";
 
 export default function CommerceHeader() {
-  const { totalItems } = useCart();
+  const { totalItems, settings } = useCart();
   return (
     <>
-      <div className="announcement"><span>⚡ Better tech, smarter upgrades</span><span>Nationwide delivery options</span><span>Secure shopping experience</span></div>
+      <div className="announcement"><span>⚡ {settings.announcementText}</span><span>Nationwide delivery options</span><span>Secure shopping experience</span></div>
       <header className="nav shell">
         <Link href="/" className="brand"><span className="brandMark">T</span><span>TECHMAN <b>AMT</b></span></Link>
         <nav className="desktopNav"><Link href="/#shop">Shop</Link><Link href="/#deals">Deals</Link><Link href="/#creator">Creator Tools</Link><Link href="/track-order">Track Order</Link></nav>
